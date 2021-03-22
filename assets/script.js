3a2514593e0d95e078225d546cc6a7f63e797de5
jayeebee <pnuthexsa311@gmail.com>
2021-03-22 17:29:19 -0400

    all card calls, icon functionality added

--- assets/script.js
+++ assets/script.js
@@ -52,10 +52,104 @@ function weatherHunt() {
             fiveDayDateEl.innerHTML = '<h4>' + fiveDayDate + '</h4>'
 
             var oneDaySky = (weatherData.list[5].weather[0].main)
-            var oneDaySkyEl = $("#oneDayFutI")
-            oneDaySkyEl.addClass("fas fa-sun")
-            console.log(oneDaySky)
+            var oneDaySkyEl = document.querySelector("#oneDayFut")
+
+            if (oneDaySky === "Clear") {
+                oneDaySkyEl.innerHTML+='<span style="color: yellow"><i class="fas   fa-sun"></i></span>'}
+
+            if (oneDaySky ==="Rain") {
+                oneDaySkyEl.innerHTML+='<span style="color: light-blue"><i class="fas fa-cloud-rain"></i></span>'}
+
+            if (oneDaySky ==="Clouds") {
+                oneDaySkyEl.innerHTML+='<span style="color: white"><i class="fas fa-cloud"></i></span>'}  
+                
+            var twoDaySky = (weatherData.list[13].weather[0].main)
+            var twoDaySkyEl = document.querySelector("#twoDayFut")
+
+            if (twoDaySky === "Clear") {
+                twoDaySkyEl.innerHTML+='<span style="color: yellow"><i class="fas   fa-sun"></i></span>'}
+
+            if (twoDaySky ==="Rain") {
+                twoDaySkyEl.innerHTML+='<span style="color: light-blue"><i class="fas fa-cloud-rain"></i></span>'}
+
+            if (twoDaySky ==="Clouds") {
+                twoDaySkyEl.innerHTML+='<span style="color: white"><i class="fas fa-cloud"></i></span>'}  
             
+            var threeDaySky = (weatherData.list[21].weather[0].main)
+            var threeDaySkyEl = document.querySelector("#threeDayFut")
+
+            if (threeDaySky === "Clear") {
+                threeDaySkyEl.innerHTML+='<span style="color: yellow"><i class="fas   fa-sun"></i></span>'}
+
+            if (threeDaySky ==="Rain") {
+                threeDaySkyEl.innerHTML+='<span style="color: light-blue"><i class="fas fa-cloud-rain"></i></span>'}
+
+            if (threeDaySky ==="Clouds") {
+                threeDaySkyEl.innerHTML+='<span style="color: white"><i class="fas fa-cloud"></i></span>'}  
+            
+            var fourDaySky = (weatherData.list[29].weather[0].main)
+            var fourDaySkyEl = document.querySelector("#fourDayFut")
+
+            if (fourDaySky === "Clear") {
+                fourDaySkyEl.innerHTML+='<span style="color: yellow"><i class="fas   fa-sun"></i></span>'}
+
+            if (fourDaySky ==="Rain") {
+                fourDaySkyEl.innerHTML+='<span style="color: light-blue"><i class="fas fa-cloud-rain"></i></span>'}
+
+            if (fourDaySky ==="Clouds") {
+                fourDaySkyEl.innerHTML+='<span style="color: white"><i class="fas fa-cloud"></i></span>'}  
+            
+            var fiveDaySky = (weatherData.list[37].weather[0].main)
+            var fiveDaySkyEl = document.querySelector("#fiveDayFut")
+
+            if (fiveDaySky === "Clear") {
+                fiveDaySkyEl.innerHTML+='<span style="color: yellow"><i class="fas   fa-sun"></i></span>'}
+
+            if (fiveDaySky ==="Rain") {
+                fiveDaySkyEl.innerHTML+='<span style="color: light-blue"><i class="fas fa-cloud-rain"></i></span>'}
+
+            if (fiveDaySky ==="Clouds") {
+                fiveDaySkyEl.innerHTML+='<span style="color: white"><i class="fas fa-cloud"></i></span>'}  
+    
+            var oneDayTemp = (weatherData.list[5].main.temp)
+            var oneDayTempEl = document.querySelector("#oneDayFut")
+            oneDayTempEl.innerHTML += '<h5>'+ "Temperature: " + Math.round(9/5*(oneDayTemp-273)+32) + "°F" +'</h5>'
+
+            var twoDayTemp = (weatherData.list[13].main.temp)
+            var twoDayTempEl = document.querySelector("#twoDayFut")
+            twoDayTempEl.innerHTML += '<h5>'+ "Temperature: " + Math.round(9/5*(twoDayTemp-273)+32) + "°F" +'</h5>'
+
+            var threeDayTemp = (weatherData.list[21].main.temp)
+            var threeDayTempEl = document.querySelector("#threeDayFut")
+            threeDayTempEl.innerHTML += '<h5>'+ "Temperature: " + Math.round(9/5*(threeDayTemp-273)+32) + "°F" +'</h5>'
+
+            var fourDayTemp = (weatherData.list[29].main.temp)
+            var fourDayTempEl = document.querySelector("#fourDayFut")
+            fourDayTempEl.innerHTML += '<h5>'+ "Temperature: " + Math.round(9/5*(fourDayTemp-273)+32) + "°F" +'</h5>'
+
+            var fiveDayTemp = (weatherData.list[37].main.temp)
+            var fiveDayTempEl = document.querySelector("#fiveDayFut")
+            fiveDayTempEl.innerHTML += '<h5>'+ "Temperature: " + Math.round(9/5*(fiveDayTemp-273)+32) + "°F" +'</h5>'
+
+            var oneDayHumidity = (weatherData.list[5].main.humidity)
+            var oneDayHumEl = document.querySelector("#oneDayFut")
+            oneDayHumEl.innerHTML += '<h4>' + "Humidity: " + oneDayHumidity + "%" +'</h4>'
+            
+            var twoDayHumidity = (weatherData.list[5].main.humidity)
+            var twoDayHumEl = document.querySelector("#twoDayFut")
+            twoDayHumEl.innerHTML += '<h4>' + "Humidity: " + twoDayHumidity + "%" +'</h4>'            
+            
+            var threeDayHumidity = (weatherData.list[5].main.humidity)
+            var threeDayHumEl = document.querySelector("#threeDayFut")
+            threeDayHumEl.innerHTML += '<h4>' + "Humidity: " + threeDayHumidity + "%" +'</h4>'           
+            
+            var fourDayHumidity = (weatherData.list[5].main.humidity)
+            var fourDayHumEl = document.querySelector("#fourDayFut")
+            fourDayHumEl.innerHTML += '<h4>' + "Humidity: " + fourDayHumidity + "%" +'</h4>'            
+            
+            var fiveDayHumidity = (weatherData.list[5].main.humidity)
+            var fiveDayHumEl = document.querySelector("#fiveDayFut")
+            fiveDayHumEl.innerHTML += '<h4>' + "Humidity: " + fiveDayHumidity + "%" +'</h4>'
 
             var longitude = (weatherData.city.coord.lon)
             var latitude = (weatherData.city.coord.lat)
